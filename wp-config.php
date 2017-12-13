97b615b1d0f7ce00b8930fa71d5722134d1faadd
Geteo12 <geteolastpiece@gmail.com>
2017-12-14 00:01:35 +0100

    envoie pdf

--- wp-config.php
+++ wp-config.php
@@ -19,24 +19,24 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'ninaslcb');
+define('DB_NAME', 'ninaslcblrstehal');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'root');
+define('DB_USER', 'ninaslcblrstehal');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'Mael2607');
 
 /** Adresse de l’hébergement MySQL. */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'ninaslcblrstehal.mysql.db');
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8');
 
 /** Type de collation de la base de données.
   * N’y touchez que si vous savez ce que vous faites.
   */
-define('DB_COLLATE', '');
+define('DB_COLLATE', 'utf8_general_ci');
 
 /**#@+
  * Clés uniques d’authentification et salage.
